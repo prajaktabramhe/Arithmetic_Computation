95b5e2707f70c2da0e74442d13031baf90072c5e
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-15 01:03:49 +0530

    Store Results in Dictionary

--- Arithmetic_Computation.sh
+++ Arithmetic_Computation.sh
@@ -17,3 +17,21 @@ echo "third equation" $equ3;
 
 equ4=$(( $a % $b + $c ))
 echo "fourth equation" $equ4;
+
+#Storing results in Dictionary
+
+declare -A  dict
+
+dict[1]=$equ1
+dict[2]=$equ2
+dict[3]=$equ3
+dict[4]=$equ4
+
+j=0
+
+echo "Printing Dictonary values";
+
+for(( j=1; j<=4; j++ ))
+do
+echo "Dict[$j]:"${dict[$j]}
+done
